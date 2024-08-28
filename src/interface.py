52675ebaed6b566ca2d63e0b2281d508abe95e4d
Marcos Correia <58613383+mdsco2@users.noreply.github.com>
2024-08-28 18:43:01 -0300

    feat: graphagent integration with streamlit

--- src/interface.py
+++ src/interface.py
@@ -1,46 +1,40 @@
 import streamlit as st
-from langchain_community.llms import Ollama
-from langchain import PromptTemplate
-from langchain.chains import LLMChain
-
-from langchain.agents import create_sql_agent
-from langchain.utilities import SQLDatabase
-from langchain_community.agent_toolkits import create_sql_agent
-from langchain_openai import ChatOpenAI
-import os
-from agent import GraphAgent
-from langchain.tools.retriever import create_retriever_tool
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List
 import requests
+from langchain_community.utilities import SQLDatabase
+from langchain_openai import ChatOpenAI
+from agent import GraphAgent
 
-st.title("Simple Chat App")
+db = SQLDatabase.from_uri("sqlite:///emendas.db")
+
+llm = ChatOpenAI(openai_api_key=st.secrets["OPENAI_API_KEY"], model="gpt-4o-mini", temperature=0)
+
+agent = GraphAgent(db, llm)
+
+st.title("Ulysses - Assistente de Pesquisa de Emendas Parlamentares")
+
+# Add disclaimer
+st.info("Disclaimer: a solução contém exclusivamente dados dos recursos destinados e empenhados via emendas parlamentares para o orçamento de 2024, abrangendo a funcional programática (função, subfunção, programa, ação e localizador), a modalidade e o grupo de natureza de despesa (GND).")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"],unsafe_allow_html=True)
+        st.markdown(message["content"], unsafe_allow_html=True)
 
-if prompt := st.chat_input("Digitar"):
-
+# Chat input
+if prompt := st.chat_input("Digite sua pergunta sobre emendas parlamentares"):
     with st.chat_message("user"):
         st.markdown(prompt)
+    
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    with st.spinner("Pensando..."):
+        response = agent.stream(prompt)
+    
+    with st.chat_message("assistant", avatar="avatar.jpg"):
+        st.markdown(response, unsafe_allow_html=True)
 
-    st.session_state.messages.append({"role":"user","content":prompt})
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
-    response = requests.post(url="http://localhost:8000/ulisses",json={"input":prompt})
-
-    with st.chat_message("assistant"):
-        st.markdown(response.content.decode("utf-8").replace('"',""),unsafe_allow_html=True)
-
-    st.session_state.messages.append({"role":"assistant","content":response.content.decode("utf-8").replace('"',"")})
-
-
-
-# Scroll to the bottom of the chat
 st.write(f"<p style='padding-bottom: 50px;'></p>", unsafe_allow_html=True)
